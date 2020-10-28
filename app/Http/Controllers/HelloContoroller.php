bc87d3c2d60b9466abf5099ff8da580a60f5ea87
mu7kata <mu7kata@icloud.com>
2020-10-28 14:15:11 +0900

    環境作成

---
+++ app/Http/Controllers/HelloContoroller.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class HelloContoroller extends Controller
+{
+    public function index(Request $request,Response $response) {
+        $html = <<<EOF
+        <html>
+        <body>
+        <h1>Hello</h1>
+        <h3>Request</h3>
+        <pre>{$request}</pre>
+        <h3>Response</h3>
+        <pre>{$response}</pre>
+        </body>
+        </html>
+        EOF;
+
+    $response->setContent($html);
+    return $response;
+   
+     
+    }
+}
