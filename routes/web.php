18716b44409adca8c969c4cca52d2c18ea23176e
mu7kata <mu7kata@icloud.com>
2020-11-30 22:37:29 +0900

    ORMの利用、person.php作成

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\HelloContoroller;
 use Illuminate\Support\Facades\Route;
 use App\Http\Middleware\HelloMiddleware;
 
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -32,3 +33,6 @@ Route::post ('hello/edit','App\Http\Controllers\HelloContoroller@update');
 Route::get ('hello/del','App\Http\Controllers\HelloContoroller@del');
 Route::post ('hello/del','App\Http\Controllers\HelloContoroller@remove');
 Route::get ('hello/show','App\Http\Controllers\HelloContoroller@show');
+Route::get ('person','App\Http\Controllers\PersonController@index');
+Route::get ('person/find','App\Http\Controllers\PersonController@find');
+Route::post ('person/find','App\Http\Controllers\PersonController@search');
