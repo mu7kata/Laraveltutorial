c4bdc55b4401d41a41d2c2560c5e68797d18da45
mu7kata <mu7kata@icloud.com>
2020-12-16 14:30:20 +0900

    編集ページ遷移ボタン作成

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -27,12 +27,14 @@
         <th><a href="/hello?sort=name">name</a></th>
         <th><a href="/hello?sort=mail">mail</a></th>
         <th><a href="/hello?sort=age">age</a></th>
+        <th>edit</th>
     </tr>
     @foreach ($items as $item)
         <tr>
             <td>{{$item->name}}</td>
             <td>{{$item->mail}}</td>
             <td>{{$item->age}}</td>
+            <td><a href="/hello/edit?id={{$item->id}}">編集</a></td>
         </tr>
     @endforeach
     </table>
